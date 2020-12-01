37e407450b5ea33d84f39ca8a6f835c10175f843
Anna Ershova <asershova@gmail.com>
2020-12-01 23:25:20 +0000

    Added prefix to configure

--- install_blast.sh
+++ install_blast.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-if [! -d "$1" ]; then 
+if [ ! -d "$1" ]; then 
 	echo "No directory $1"
 	exit
 fi
-wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.10.1/ncbi-blast-2.10.1+-src.tar.gz
-tar -xvzf ncbi-blast-2.10.1+-src.tar.gz
-cd ncbi-blast-2.10.1+-src
+blast=ncbi-blast-2.10.1+-src.tar.gz
+
+if [ ! -f "$blast" ]; then
+       wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.10.1/$blast
+fi
+if [ -d "${blast%%.tar.gz}" ]; then
+	rm -rf "${blast%%.tar.gz}"
+fi
+tar -xvzf $blast
+cd ${blast%%.tar.gz}
 cd c++
 ./configure --prefix=$1
 cd ReleaseMT/build
 make all_r
+cp -rf ../bin/* $1
